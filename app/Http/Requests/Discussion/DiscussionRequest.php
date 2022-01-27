642861f6a80132829daa805cd4f32da48569054c
ardi renaldi <renaldimedia@gmail.com>
2022-01-27 17:24:08 +0700

    add param todiscussion

--- app/Http/Requests/Discussion/DiscussionRequest.php
+++ app/Http/Requests/Discussion/DiscussionRequest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Http\FormRequest;
  * @bodyParam title string required judul diskusi. Example: diskusi tentang rental mobil
  * @bodyParam description string deskripsi diskusi.
  * @bodyParam cover_image string cover image diskusi.
+ * @bodyParam discussion_type string optional (default forum) isi dengan forum/chat
+ * @bodyParam private numeric optional (default 1) isi dengan 1 / 0
  */
 class DiscussionRequest extends FormRequest
 {
@@ -22,6 +24,8 @@ class DiscussionRequest extends FormRequest
             'title' => 'required|string',
             'description' => 'nullable|string',
             'cover_image' => 'nullable|string',
+            'discussion_type' => 'nullabel|string',
+            'private' => 'nullable|numeric'
         ];
     }
 }
