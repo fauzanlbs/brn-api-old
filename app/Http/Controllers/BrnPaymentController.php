a429dc9d1b5085743be97b0baa5e14d34944f41e
ardi renaldi <renaldimedia@gmail.com>
2022-01-30 19:34:36 +0700

    test

--- app/Http/Controllers/BrnPaymentController.php
+++ app/Http/Controllers/BrnPaymentController.php
@@ -59,13 +59,13 @@ class BrnPaymentController extends Controller
                     return $q->whereYear('created_at', $year);
                 })->when($korda, function($q, $korda){
                     if(!is_array($korda)){
-                        return $q->where('user_personal_informations.korda_id', $korda);
+                        return $q->filterKorda($korda);
                     }else{
                         foreach($korda as $i => $kor){
                             if($i == 0){
-                                $q = $q->where('user_personal_informations.korda_id', $kor);
+                                $q = $q->filterKorda($kor);
                             }else{
-                                $q = $q->orWhere('user_personal_informations.korda_id', $kor);
+                                $q = $q->filterKorda($kor);
                             }
                         }
 
