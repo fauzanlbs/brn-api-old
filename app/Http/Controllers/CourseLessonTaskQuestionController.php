bb196efee1ff20554aa8033ea863bfae92afdf21
ardi renaldi <renaldimedia@gmail.com>
2022-02-17 15:54:33 +0700

    edit

--- app/Http/Controllers/CourseLessonTaskQuestionController.php
+++ app/Http/Controllers/CourseLessonTaskQuestionController.php
@@ -56,16 +56,16 @@ class CourseLessonTaskQuestionController extends Controller
      *
      * @group Kursus
      *
-     * @queryParam coursesid integer required Penyortiran berdasarkan id courses. Example: 1
+     * @urlParam coursesid integer required Penyortiran berdasarkan id courses. Example: 1
      *
      * @param Request $request
-     *
+     * @param Course $course
      *
      * @responseFile storage/responses/course-lesson-task-resource.response.json
      */
     public function getCourseLessonTaskQuestionWhereLevel(Request $request, Course $course)
     {
-        print_r('courses task list');exit;
+        // print_r('courses task list');exit;
         $uid = $request->user()->id;
 
         $alreadyEnrolled = $course->students()->where('user_id', $uid)->exists();
@@ -73,16 +73,16 @@ class CourseLessonTaskQuestionController extends Controller
             return $this->responseMessage('Anda harus mengikuti kursus dari pembelajaran/video ini terlebih dahulu sebelum melihat komentar.');
         }
 
-        $coursesid = $request->query('coursesid');
-        if (!$coursesid) {
-            return $this->responseMessage('Course ID dibutuhkan!');
-        }
+        // $coursesid = $request->query('coursesid');
+        // if (!$coursesid) {
+        //     return $this->responseMessage('Course ID dibutuhkan!');
+        // }
         
 
         $tasks = DB::table('course_lesson_task_questions')
                 ->join('course_lessons', 'course_lesson_task_questions.id', '=', 'course_lessons.course_lesson_id', 'left')
                 ->join('courses', 'courses.id', '=', 'course_lesson_task_questions.course_id', 'left')
-                ->where('courses.id', $coursesid)->get();
+                ->where('courses.id', $course->id)->get();
 
         // $tasks = CourseLessonTaskQuestion::whereHas('courseLesson', function ($q) use ($coursesid) {
         //     $q->where('course_id', $coursesid);
